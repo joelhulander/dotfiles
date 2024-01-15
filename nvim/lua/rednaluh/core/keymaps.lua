8a7528b2332a5df1d12f9686fd52ed91c2dc4636
Joel Hulander <joel.hulander@sweetsystems.se>
2024-01-15 17:23:58 +0100

    Add EE as ESC keymap

--- nvim/lua/rednaluh/core/keymaps.lua
+++ nvim/lua/rednaluh/core/keymaps.lua
@@ -99,6 +99,7 @@ keymap.set({ "n", "v" }, "e", "k", opts)
 keymap.set({ "n", "v" }, "E", "K", opts)
 keymap.set({ "n", "v" }, "j", "e", opts)
 keymap.set({ "n", "v" }, "J", "e", opts)
+keymap.set("i", "EE", "<ESC>", opts )
 
 -- oil
 keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory", noremap = true, silent = true })
