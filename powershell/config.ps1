ad2e4862e8fec89f0d5b658b9a807a5041a4551d
Joel Hulander <joel.hulander@sweetsystems.se>
2024-12-02 12:18:08 +0100

    Fix current working dir issue for starship

--- powershell/config.ps1
+++ powershell/config.ps1
@@ -1,5 +1,4 @@
 $env:Path += ';C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE'
-$Host.UI.RawUI.WindowTitle = 'Terminal'
 $env:LC_MESSAGES="en-US"
 $env:TERM='xterm-256color'
 $Env:KOMOREBI_CONFIG_HOME = "${Env:USERPROFILE}\.config\komorebi"
@@ -247,19 +246,30 @@ function branch {
 	return $branch;
 }
 
-function Set-EnvVar
-{
-  $p = $executionContext.SessionState.Path.CurrentLocation
-  $osc7 = ""
-  if ($p.Provider.Name -eq "FileSystem")
-  {
-    $ansi_escape = [char]27
-    $provider_path = $p.ProviderPath -Replace "\\", "/"
-    $osc7 = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}${ansi_escape}\"
-  }
-  $env:OSC7=$osc7
+# function Set-EnvVar
+# {
+#   $p = $executionContext.SessionState.Path.CurrentLocation
+#   $osc7 = ""
+#   if ($p.Provider.Name -eq "FileSystem")
+#   {
+#     $ansi_escape = [char]27
+#     $provider_path = $p.ProviderPath -Replace "\\", "/"
+#     $osc7 = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}${ansi_escape}\"
+#   }
+#   $env:OSC7=$osc7
+# }
+# New-Alias -Name 'Set-PoshContext' -Value 'Set-EnvVar' -Scope Global -Force
+
+$prompt = ""
+function Invoke-Starship-PreCommand {
+    $current_location = $executionContext.SessionState.Path.CurrentLocation
+    if ($current_location.Provider.Name -eq "FileSystem") {
+        $ansi_escape = [char]27
+        $provider_path = $current_location.ProviderPath -replace "\\", "/"
+        $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
+    }
+    $host.ui.Write($prompt)
 }
-New-Alias -Name 'Set-PoshContext' -Value 'Set-EnvVar' -Scope Global -Force
 
 # Invoke-Expression (&starship init powershell)
 # $omp_config = "~/.dotfiles/ohmyposh/catppuccin.yaml"
