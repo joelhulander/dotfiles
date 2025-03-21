7697ffc6b0ff7a876a2f6e153cef1b266a1639c7
Joel Hulander <joel.hulander@sweetsystems.se>
2025-03-21 16:31:53 +0100

    Add mini.nvim plugin

---
+++ nvim/lua/rednaluh/plugins/mini.lua
@@ -0,0 +1,25 @@
+return {
+	'echasnovski/mini.nvim', version = false,
+	config = function()
+		require('mini.diff').setup(
+			{
+				view = {
+					style = 'sign',
+				},
+				mappings = {
+					apply = '<leader>gh',
+					reset = '<leader>gH',
+
+					textobject = '<leader>gh',
+
+					goto_first = '<leader>[H',
+					goto_prev = '<leader>[h',
+					goto_next = '<leader>]h',
+					goto_last = '<leader>]H',
+				},
+			}
+		);
+		require('mini.git').setup()
+	end
+}
+
